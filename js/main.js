54c0b1cf5cf08ad7d70342994e0bf1c2a2201df2
Unknown <aluno16811@ipt.pt>
2019-02-14 16:48:43 +0000

    Melhorias na Navbar, Banner e About + Seccção dos Prémios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   showMenu();
-
+  checkScrollFromTop();
   // função para esconder o menu mobile quando é aumentada a largura da página (caso seja superior a 1023 pixeis)
   $(window).resize(function() {
     if ($(this).width() > 1023 && $('.menu_toggle').hasClass('is-clicked')) {
@@ -11,12 +11,20 @@ $( document ).ready(function() {
 
   // É adicionada cor gradualmente à navbar durante o scroll da página (o backgroud da navbar por default é transparente)
   $(window).scroll(function() {
-    if (!$("#navbar").hasClass('mobile')){
-      banner = $("#banner").height() - 100;
-      alpha = $(document).scrollTop()/banner;
-      $("#navbar").css({'background-color':'rgba(130,37,108,'+ alpha +')'});
+    if (!$('#navbar').hasClass('mobile')){
+      if ($(document).scrollTop() > 50) {
+        $('#navbar').addClass('with-background');
+        $("#navbar_logo").attr("src","./images/logo-cityhack-preto.png");
+      } else {
+        $('#navbar').removeClass('with-background');
+        $("#navbar_logo").attr("src","./images/logo-cityhack-branco.png");
+      }
     }
   });
+
+  if ($(window).width() > 800){
+    particlesJS.load('particles-js', 'particles.json');
+  }
 });
 
 // função para mostrar/esconder o menu em mobile
@@ -24,17 +32,25 @@ function showMenu(){
   $('.menu_toggle').bind('click', function () {
     $('.menu_toggle').toggleClass('is-clicked');
     $('#navbar').toggleClass('mobile');
-    if ($("#navbar").hasClass('mobile')) {
-      $("#navbar").css({'background-color':'black'});
-    } else {
-      banner = $("#banner").height() - 100;
-      alpha = $(document).scrollTop()/banner;
-      if (alpha > 1) {
-        $("#navbar").css({'background-color':'rgba(130,37,108,1)'});
-      } else {
-        $("#navbar").css({'background-color':'rgba(130,37,108,'+ alpha +')'});
-      }
 
+    if ($('.menu_toggle').hasClass('is-clicked')){
+      $("#navbar_logo").attr("src","./images/logo-cityhack-preto.png");
+    } else {
+      if ($(document).scrollTop() > 50) {
+        $("#navbar_logo").attr("src","./images/logo-cityhack-preto.png");
+      } else {
+        $("#navbar_logo").attr("src","./images/logo-cityhack-branco.png");
+      }
     }
   });
+}
+
+function checkScrollFromTop(){
+  if ($(document).scrollTop() > 50) {
+    $('#navbar').addClass('with-background');
+    $("#navbar_logo").attr("src","./images/logo-cityhack-preto.png");
+  } else {
+    $('#navbar').removeClass('with-background');
+    $("#navbar_logo").attr("src","./images/logo-cityhack-branco.png");
+  }
 }
