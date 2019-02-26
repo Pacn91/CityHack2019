50ae27fe11a522014021f9ec6bdee532fb8ed516
Unknown <aluno16811@ipt.pt>
2019-02-26 12:21:05 +0000

    Eventos Anteriores restyled + outras coisas

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
   showMenu();
   checkScrollFromTop();
-  openGallery();
+  openFancyBoxes();
   fitText();
+  smoothScroll();
   // função para esconder o menu mobile quando é aumentada a largura da página (caso seja superior a 1023 pixeis)
   $(window).resize(function() {
     if ($(this).width() > 1023 && $('.menu_toggle').hasClass('is-clicked')) {
@@ -27,6 +28,16 @@ $( document ).ready(function() {
   if ($(window).width() > 800){
     particlesJS.load('particles-js', 'particles.json');
   }
+
+  $('.menu a').bind('click', function () {
+    $('#navbar').removeClass('mobile');
+    $('.menu_toggle').removeClass('is-clicked');
+  })
+
+  // Faz com que o botão de "fechar" nunca desapareça.
+  $('#google_form').fancybox({
+    idleTime: false
+  });
 });
 
 // função para mostrar/esconder o menu em mobile
@@ -58,12 +69,13 @@ function checkScrollFromTop(){
 }
 
 function fitText(){
-  $(".previousEditionsTitle").fitText(1.1, { minFontSize: '50px', maxFontSize: '95px' });
+  // $(".previousEditionsTitle").fitText(.3, { minFontSize: '10px', maxFontSize: '95px' });
 }
 
-function openGallery(){
+function openFancyBoxes(){
 
-  $('#2018gallery').on('click', function() {
+  // Abrir a galeria do evento de 2018
+  $('.2018gallery').on('click', function() {
     $.fancybox.open([
       {
         src  : './images/cityhack2018/1.jpg',
@@ -150,7 +162,9 @@ function openGallery(){
       }
     })
   })
-  $('#2017gallery').on('click', function() {
+
+  // Abrir a galeria do evento de 2017
+  $('.2017gallery').on('click', function() {
     $.fancybox.open([
       {
         src  : './images/cityhack2018/1.jpg',
@@ -237,4 +251,88 @@ function openGallery(){
       }
     })
   })
+
+  // Abrir os vídeos do evento de 2018
+  $('.2018teaser').on('click', function() {
+    $.fancybox.open([
+      {
+        src  : 'https://www.youtube.com/watch?v=CuXTIRZVA3w',
+      },
+      {
+        src  : 'https://www.youtube.com/watch?v=Cko711pVWQM',
+      },
+    ], {
+      thumbs : {
+        autoStart : true
+      }
+    })
+  })
+
+  // Abrir os vídeos do evento de 2017
+  $('.2017teaser').on('click', function() {
+    $.fancybox.open([
+      {
+        src  : 'https://www.youtube.com/watch?v=RexN0GsiIqQ',
+      },
+      {
+        src  : 'https://www.youtube.com/watch?v=Q43mk1_uHK8',
+      },
+      {
+        src  : 'https://www.youtube.com/watch?v=RUXvR0sZU-Y',
+      },
+    ], {
+      thumbs : {
+        autoStart : true
+      }
+    })
+  })
+
+
+}
+
+function smoothScroll(){
+    // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // Grab height of fixed header
+      var headHeight = $('#navbar').outerHeight();
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - headHeight + 1
+          }, 1000, function() {
+            // Callback after animation
+            // Don't scroll on focus!
+              $.fn.focusNoScroll = function(){
+                var x = window.scrollX, y = window.scrollY;
+                this.focus();
+                window.scrollTo(x, y);
+                return this; //chainability
+
+              };
+              // Must change focus, but don't scroll!
+            var $target = $(target);
+            $target.focusNoScroll();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focusNoScroll(); // Set focus again
+            };
+          });
+        }
+      }
+    });
 }
